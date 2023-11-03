4f2b83afb765379e1fb756686a4d4ab5012732d9
minchonang <green8085@naver.com>
2023-11-03 14:55:59 +0900

    11월 3일

--- python/test.py
+++ python/test.py
@@ -1,9 +1,18 @@
-# 1 + 1
-# print(1 + 1)
+score = [10.0, 9.0, 8.3, 7.1, 3.0, 9.0]
+temp = 0
+i = 0
+while i < len(score) - 1:
+   if score[i] < score[i + 1]:
+      temp = score[i]
+      score[i] = score[i + 1]
+      score[i + 1] = temp
+   i += 1
 
-x = input('입력하세요: ')
-if int(x) > 10:
-   print(x, type(x))
-else:
-   print('틀렸습니다')
+
+i = 1
+sum = 0
+while i < 5:
+   sum += score[i]
+result = sum / 4
+print(result)
 
