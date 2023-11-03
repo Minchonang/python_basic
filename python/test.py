89819479ada5a21be618d51f0da519a90b82024f
minchonang <green8085@naver.com>
2023-11-03 19:42:35 +0900

    11월 3일

--- python/test.py
+++ python/test.py
@@ -1,18 +1,28 @@
-score = [10.0, 9.0, 8.3, 7.1, 3.0, 9.0]
-temp = 0
-i = 0
-while i < len(score) - 1:
-   if score[i] < score[i + 1]:
-      temp = score[i]
-      score[i] = score[i + 1]
-      score[i + 1] = temp
-   i += 1
+score = [
+    10.0,
+    9.0,
+    8.3,
+    7.1,
+    3.0,
+    9.0
+]
+
+for i in range(5):
+    for j in range(5):
+        if score[j] < score[j + 1]:
+            temp = score[j]
+            score[j] = score[j + 1]
+            score[j + 1] = temp
+
+print(f'정렬된 점수: { score }')
 
 
-i = 1
 sum = 0
-while i < 5:
-   sum += score[i]
-result = sum / 4
-print(result)
+for i in range(1, 5):
+    sum += score[i]
 
+result = sum / 4
+
+print(
+    f'최고 점수: { max(score) }, 최저 점수: { min(score) }\n평균: { result }점'
+)
